a2f9acc9c5a19a23c43c0c215145851da16d379a
Lesuz <50194165+Lesuz@users.noreply.github.com>
2020-10-26 19:49:36 +0200

    Added user input

--- javaScript.js
+++ javaScript.js
@@ -13,8 +13,35 @@ window.onload = function() {
         var taskToDo = inputTodo.value ;
         var doneByDate = inputDate.value;
 
-        var myTable = document.getElementsByClassName("TodoTable")[0];
-        var newTable = myTable.cloneNode(true);
-        document.body.appendChild(newTable);
+        // create table element
+        var table = document.createElement('table');
+        table.style.marginTop = "10px" ;
+        table.style.marginBottom = "10px" ;
+        // add a row element into the table
+        var row = table.insertRow();
+        // add the first cell element into the row
+        var cell = row.insertCell();
+
+        // Add input into the cells
+        // first cell has the arrow image
+        cell.innerHTML = '<img class ="arrows" src="images/arrowdown.png" alt="Arrowdown">';
+        // Adding a couple styles
+        cell.style.backgroundColor = "#fff2fe";
+        cell.style.borderStyle = "hidden";
+        // add second cell with the task
+        cell = row.insertCell();
+        cell.innerHTML = taskToDo;
+        // add the third cell with the date
+        cell = row.insertCell();
+        cell.innerHTML = doneByDate;
+        // add the fourth cell with the image to delete the task
+        cell = row.insertCell();
+        cell.innerHTML = '<img class ="delete" src="images/error.png" alt="delete">';
+        // Adding a couple styles
+        cell.style.backgroundColor = "#fff2fe";
+        cell.style.borderStyle = "hidden";
+
+        // appned the table into the div with the classname of TodoTable
+        document.getElementsByClassName("TodoTable")[0].appendChild(table);
     });
 }
