35ab6bc1d40313cb2e14180152503f58b6a3720b
Lesuz <50194165+Lesuz@users.noreply.github.com>
2020-10-28 18:27:29 +0200

    Added delete function

--- javaScript.js
+++ javaScript.js
@@ -4,8 +4,11 @@ window.onload = function() {
     var buttonAdd = document.getElementById("add");
     var inputTodo = document.getElementById("todo");
     var inputDate = document.getElementById("date");
-    var deleteButton = document.getElementsByClassName("delete");
+    var errormessage = document.getElementById("warning");
+    var buttonDelete = document.getElementsByClassName("delete");
     var moreButton = document.getElementsByClassName("arrows");
+    // var deleteBut = document.getElementById("delete");
+    var wholeTable = document.getElementById("TodoTable");
 
     // Add button event listener
     buttonAdd.addEventListener("click", function() {
@@ -13,42 +16,64 @@ window.onload = function() {
         var taskToDo = inputTodo.value ;
         var doneByDate = inputDate.value;
 
-        // create table element
-        var table = document.createElement('table');
-        table.style.marginTop = "10px" ;
-        table.style.marginBottom = "10px" ;
-        // add a row element into the table
-        var row = table.insertRow();
-        // add the first cell element into the row
-        var cell = row.insertCell();
+        //var date = new Date(inputDate);
 
-        // Add input into the cells
-        // first cell has the arrow image
-        cell.innerHTML = '<img class ="arrows" src="images/arrowdown.png" alt="Arrowdown">';
+        errormessage.style.display = "none";
 
-        // Adding a couple styles
-        cell.style.backgroundColor = "#fff2fe";
-        cell.style.borderStyle = "hidden";
+        if (taskToDo === "" || taskToDo === " ") {
+            errormessage.style.display = "block";
+            errormessage.innerHTML = "Cannot add an empty task to the list!";
+        }else if (!doneByDate){
+            errormessage.style.display = "block";
+            errormessage.innerHTML = "Cannot add a task without a done by date!";
+        }else{
 
-        // add second cell with the task
-        cell = row.insertCell();
-        cell.innerHTML = taskToDo;
-        cell.style.paddingRight = "30px";
 
-        // add the third cell with the date
-        cell = row.insertCell();
-        cell.innerHTML = doneByDate;
-        cell.style.width = "200px";
+            // create table element
+            var table = document.createElement('table');
+            table.style.marginTop = "10px" ;
+            table.style.marginBottom = "10px" ;
+            // add a row element into the table
+            var row = table.insertRow();
+            // add the first cell element into the row
+            var cell = row.insertCell();
 
-        // add the fourth cell with the image to delete the task
-        cell = row.insertCell();
-        cell.innerHTML = '<img class ="delete" src="images/error.png" alt="delete">';
-        
-        // Adding a couple styles
-        cell.style.backgroundColor = "#fff2fe";
-        cell.style.borderStyle = "hidden";
+            // Add input into the cells
+            // first cell has the arrow image
+            cell.innerHTML = '<img class ="arrows" src="images/arrowdown.png" alt="Arrowdown">';
 
-        // appned the table into the div with the classname of TodoTable
-        document.getElementsByClassName("TodoTable")[0].appendChild(table);
+            // Adding a couple styles
+            cell.style.backgroundColor = "#fff2fe";
+            cell.style.borderStyle = "hidden";
+
+            // add second cell with the task
+            cell = row.insertCell();
+            cell.innerHTML = taskToDo;
+            cell.style.paddingRight = "30px";
+
+            // add the third cell with the date
+            cell = row.insertCell();
+            cell.innerHTML = doneByDate;
+            cell.style.width = "200px";
+
+            // add the fourth cell with the image to delete the task
+            cell = row.insertCell();
+            cell.innerHTML = '<button class="delete">Delete</button>';
+            cell.addEventListener("click", function(){
+                deleteTask(this)
+            });
+            
+            // Adding a couple styles
+            cell.style.backgroundColor = "#fff2fe";
+            cell.style.borderStyle = "hidden";
+
+            // append the table
+            wholeTable.appendChild(table);
+        }
     });
+
+    function deleteTask(elem) {
+        var taskTable = elem.parentNode.parentNode.parentNode
+        taskTable.parentNode.removeChild(taskTable);
+    }
 }
