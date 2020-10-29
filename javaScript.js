b59e3b97448b98ebe674b510903f65473bc52578
Lesuz <50194165+Lesuz@users.noreply.github.com>
2020-10-29 19:21:24 +0200

    Modified css, added pull down menu

--- javaScript.js
+++ javaScript.js
@@ -4,10 +4,10 @@ window.onload = function() {
     var buttonAdd = document.getElementById("add");
     var inputTodo = document.getElementById("todo");
     var inputDate = document.getElementById("date");
+    var inputImportance = document.getElementById("selection");
     var errormessage = document.getElementById("warning");
     var buttonDelete = document.getElementsByClassName("delete");
     var moreButton = document.getElementsByClassName("arrows");
-    // var deleteBut = document.getElementById("delete");
     var wholeTable = document.getElementById("TodoTable");
 
     // Add button event listener
@@ -15,8 +15,6 @@ window.onload = function() {
         // get input values   
         var taskToDo = inputTodo.value ;
         var doneByDate = inputDate.value;
-
-        //var date = new Date(inputDate);
 
         errormessage.style.display = "none";
 
@@ -27,45 +25,48 @@ window.onload = function() {
             errormessage.style.display = "block";
             errormessage.innerHTML = "Cannot add a task without a done by date!";
         }else{
-
+            
+            var importanceValue = inputImportance.options[inputImportance.selectedIndex].value;
 
             // create table element
             var table = document.createElement('table');
             table.style.marginTop = "10px" ;
             table.style.marginBottom = "10px" ;
+
+            console.log(importanceValue);
+            if(importanceValue === "low"){
+                table.style.backgroundColor = "#a6dba6";
+            }else if(importanceValue === "medium"){
+                table.style.backgroundColor = "#fffab7";
+            }else if(importanceValue === "high"){
+                table.style.backgroundColor = "#ff7373";
+            }
             // add a row element into the table
             var row = table.insertRow();
             // add the first cell element into the row
             var cell = row.insertCell();
 
-            // Add input into the cells
-            // first cell has the arrow image
-            cell.innerHTML = '<img class ="arrows" src="images/arrowdown.png" alt="Arrowdown">';
-
-            // Adding a couple styles
-            cell.style.backgroundColor = "#fff2fe";
-            cell.style.borderStyle = "hidden";
-
-            // add second cell with the task
-            cell = row.insertCell();
+            // add the the text into the cell
             cell.innerHTML = taskToDo;
             cell.style.paddingRight = "30px";
+            cell.style.width = "60%";
 
-            // add the third cell with the date
+            // add the second cell with the date
             cell = row.insertCell();
             cell.innerHTML = doneByDate;
             cell.style.width = "200px";
 
-            // add the fourth cell with the image to delete the task
+            // add the third cell with the image to delete the task
             cell = row.insertCell();
-            cell.innerHTML = '<button class="delete">Delete</button>';
+            cell.innerHTML = '<img src="images/error.png" alt="delete" class="delete">';
             cell.addEventListener("click", function(){
                 deleteTask(this)
             });
             
             // Adding a couple styles
-            cell.style.backgroundColor = "#fff2fe";
             cell.style.borderStyle = "hidden";
+            cell.style.width="5%";
+            cell.style.background = "none";
 
             // append the table
             wholeTable.appendChild(table);
@@ -76,4 +77,5 @@ window.onload = function() {
         var taskTable = elem.parentNode.parentNode.parentNode
         taskTable.parentNode.removeChild(taskTable);
     }
+
 }
